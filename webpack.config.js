910d4dea5c3057d8ebb2a3fa4996e26e68c81eb1
glory-ka <57584382+glory-ka@users.noreply.github.com>
2022-03-08 22:44:04 +0000

    copy webpack config from monaco editor github

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'eval-source-map',
-    entry : './src/js/ide.js',
-    output : {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    }
+	entry: './src/js/ide.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'app.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.ttf$/,
+				use: ['file-loader']
+			}
+		]
+	},
+	plugins: [new MonacoWebpackPlugin()]
 };
